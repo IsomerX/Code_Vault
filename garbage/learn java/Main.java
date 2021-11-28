de07a607def69e2c5376a93e61ce3eeac859f258
IsomerX <49413460+IsomerX@users.noreply.github.com>
2021-11-28 11:13:39 +0530

    28 nov.1

---
+++ garbage/learn java/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    static void func(String a){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        // String a = "123dajoiajsd";
+        // System.out.println(a);
+        // System.out.println(a.length());
+        // System.out.println(args);
+
+        // String text = "This is a sample text lol";
+        // System.out.println(text.indexOf("text"));
+
+        int[] arra = { 1,2,3,4,5,6,12 };
+        System.out.println(arra.length);
+        func("this is asd");
+    }
+}
